6cd5c8753505fc965b8a6a59756793244d06000c
surmad8-hash <surmad8@gmail.com>
2026-09-21 10:44:10 +0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // عدّل هذا السطر برابط تطبيق الويب (Apps Script) الخاص بك بعد النشر.
 // يجب أن ينتهي الرابط بـ /exec
-const APPS_SCRIPT_URL = 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwMUnHUx4sbx_hQ1AotNIgahjldmFffWliniT8MgKpYaGzBwK6V6OCWtAkKONViGrP8/exec';
 
 // رمز دخول لوحة الإدارة (حماية بسيطة على مستوى الواجهة فقط،
 // وليست بديلاً عن ضبط صلاحيات المشاركة في جوجل شيت نفسه).
